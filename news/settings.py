bf59616f0cb97312c26befa727b7ae8033208e26
aleeIsaev <75799767+aleeIsaev@users.noreply.github.com>
2021-07-31 15:57:33 +0600

    rollback

--- news/settings.py
+++ news/settings.py
@@ -13,9 +13,9 @@ import os
 from pathlib import Path
 
 import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,8 +27,6 @@ SECRET_KEY = 'django-insecure--t_*w6sl2ppj@37r)(v&bz4xang%j1k8v#p=600@^14&n!o6fo
 DEBUG = True
 
 ALLOWED_HOSTS = ['hackatonnewsapp.herokuapp.com', '127.0.0.1']
-CORS_ORIGIN_ALLOW_ALL = True
-
 
 # Application definition
 
@@ -44,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'django_filters',
-    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'news.urls'
@@ -79,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'news.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -89,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -109,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -122,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
